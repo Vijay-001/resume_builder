5e17f16652da8a820d11983dba9d197887f81afc
Vijay <Vijaykulkarni43@gmail.com>
2021-10-18 17:57:34 +0530

    Added Interface and implmented useState hook

--- src/containers/userDetails/personalDetails.tsx
+++ src/containers/userDetails/personalDetails.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import InputField from "../../common/formValidation/formFields/inputFields";
+import { useState } from "react";
+import { UserDetails } from "../../common/userInterface/userDetails";
 
 export default function UserPersonalDetails(props: any) {
   const {
@@ -9,13 +11,21 @@ export default function UserPersonalDetails(props: any) {
       firstName,
       lastName,
       address1,
-      address2,
       city,
       state,
       zipcode,
       country,
     },
   } = props;
+
+    const [userInfoState, setUserInfoState] = useState({});
+  const setUserDetails = (fieldName: string, value: string) => {
+    setUserInfoState({
+      ...userInfoState,
+      [fieldName]: value,
+    });
+  };
+
   return (
     <React.Fragment>
       <p></p>
@@ -26,28 +36,74 @@ export default function UserPersonalDetails(props: any) {
 
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <InputField name={firstName.name} label={firstName.label} fullWidth />
+          <InputField
+            name={firstName.name}
+            label={firstName.label}
+            onChange={(event: any) => {
+              setUserDetails("firstName", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={lastName.name} label={lastName.label} fullWidth />
+          <InputField
+            name={lastName.name}
+            label={lastName.label}
+            onChange={(event: any) => {
+              setUserDetails("lastName", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={address1.name} label={address1.label} fullWidth />
+          <InputField
+            name={address1.name}
+            label={address1.label}
+            onChange={(event: any) => {
+              setUserDetails("address1", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={address2.name} label={address2.label} fullWidth />
+          <InputField
+            name={state.name}
+            label={state.label}
+            onChange={(event: any) => {
+              setUserDetails("state", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={state.name} label={state.label} fullWidth />
+          <InputField
+            name={city.name}
+            label={city.label}
+            onChange={(event: any) => {
+              setUserDetails("city", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={city.name} label={city.label} fullWidth />
+          <InputField
+            name={country.name}
+            label={country.label}
+            onChange={(event: any) => {
+              setUserDetails("country", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
-          <InputField name={country.name} label={country.label} fullWidth />
-        </Grid>
-        <Grid item xs={12}>
-          <InputField name={zipcode.name} label={zipcode.label} fullWidth />
+          <InputField
+            name={zipcode.name}
+            label={zipcode.label}
+            onChange={(event: any) => {
+              setUserDetails("zipcode", event.target.value);
+            }}
+            fullWidth
+          />
         </Grid>
       </Grid>
     </React.Fragment>
